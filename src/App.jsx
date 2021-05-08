acaa4a163f285fa6f24b7fa5ccca02e967aba2ae
Sebastián Jiménez <juansebas257@gmail.com>
2021-05-08 18:26:58 -0500

    update calculator

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,34 @@ import Button from './components/Button';
 import Result from './components/Result';
 import './App.css';
 import MathOperations from './components/MathOperations';
+import Functions from './components/Functions';
 
-const App = () => {
-    //debugger;
-    console.log('rendering app');
-    return (
-        <main className='react-calculator'>
-            <Result />
-            <div className="numbers">
-                <Button text="1" clickHandler={pressButton}></Button>
-                <Button text="2" clickHandler={pressButton}></Button>
-                <Button text="3" clickHandler={pressButton}></Button>
-                <Button text="4" clickHandler={pressButton}></Button>
-                <Button text="5" clickHandler={pressButton}></Button>
-                <Button text="6" clickHandler={pressButton}></Button>
-                <Button text="7" clickHandler={pressButton}></Button>
-                <Button text="8" clickHandler={pressButton}></Button>
-                <Button text="9" clickHandler={pressButton}></Button>
-                <Button text="0" clickHandler={pressButton}></Button>
-            </div>
+class App extends Component {
+    render() {
+        //debugger;
+        console.log('rendering app');
+        return (
+            <main className='react-calculator'>
+                <Result />
+                <div className="numbers">
+                    <Button text="1" clickHandler={pressButton}></Button>
+                    <Button text="2" clickHandler={pressButton}></Button>
+                    <Button text="3" clickHandler={pressButton}></Button>
+                    <Button text="4" clickHandler={pressButton}></Button>
+                    <Button text="5" clickHandler={pressButton}></Button>
+                    <Button text="6" clickHandler={pressButton}></Button>
+                    <Button text="7" clickHandler={pressButton}></Button>
+                    <Button text="8" clickHandler={pressButton}></Button>
+                    <Button text="9" clickHandler={pressButton}></Button>
+                    <Button text="0" clickHandler={pressButton}></Button>
+                </div>
 
-            <div className="functions">
-                <Button text="clear"></Button>
-                <Button text="r"></Button>
-            </div>
-            <MathOperations onClickOperation={operate} onClickEqual={functionEqual} />
-        </main>
-    )
+                <Functions onContentClear={onContentClear} onDelete={onDelete}></Functions>
+                <MathOperations onClickOperation={operate} onClickEqual={functionEqual} />
+            </main>
+        )
+    }
+
 }
 
 const operate = (operation) => {
@@ -40,8 +41,16 @@ const functionEqual = () => {
     console.log('equals');
 }
 
-const pressButton = () => {
-    console.log('press button')
+const pressButton = (number) => {
+    console.log('press button', number)
+}
+
+const onContentClear = () => {
+    console.log('clear');
+}
+
+const onDelete = () => {
+    console.log('deleteNumber');
 }
 
 export default App;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Button.css'
 
 const Button = ({ type, text, clickHandler }) => (
-    <button className={type} onClick={clickHandler(text)}>
+    <button className={type} onClick={() => clickHandler(text)}>
         {text}
     </button>
 )
